69cb47222d9bf2fc24cd07fcfb1a6c5b3c9d3126
Emanuel Toledo <ematolr@gmail.com>
2020-06-23 18:31:42 -0500

    seeded with spoonacular

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,4 @@
 class Recipe < ApplicationRecord
-  has_many :reviews 
-  has_many :users, through: :reviews
-  has_many :categories, through: :recipe_category
-  has_many :ingredients, through: :portions
-  belongs_to :lists, through: :list_recipes
+  has_many :reviews
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'pry-byebug'
+SPOONACULAR_KEY = "9242419348644293bdd2777dbdf49ac5"
+url = "https://api.spoonacular.com/recipes/random?number=100&apiKey=#{SPOONACULAR_KEY}"
+recipes_request = HTTParty.get(url)
+
+spoonacular_recipes_params = recipes_request["recipes"]
+spoonacular_recipes_params.each do |sr_params|
+  # columns               keys on spoonacular
+  Recipe.create!(
+    title:        sr_params["title"], # title
+    instructions: sr_params["instructions"], # instructions
+    prep_time:    sr_params["readyInMinutes"], # prep_time
+    servings:     sr_params["servings"], # servings
+    source_id:    sr_params["id"], # source_url >>>> change to source_id ??? 1098388
+    image:        sr_params["image"], # image
+    vegetarian:   sr_params["vegetarian"], # summary
+    gluten_free:  sr_params["gluten_free"],
+    vegan:        sr_params["vegan"],
+    dairy_free:   sr_params["dairy_free"],
+    very_healthy: sr_params["very_healthy"],
+    cheap:        sr_params["cheap"],
+    very_popular: sr_params["very_popular"],
+    ketogenic:    sr_params["ketogenic"]
+  )
+end 
