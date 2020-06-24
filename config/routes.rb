f6c9eab1d4f1243bedc25795fc806b7a2785c813
Bhanu Gummadi <prakash@codejek.com>
2020-06-24 14:37:51 -0700

    controller actions for #show and #random

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :recipes, only: ['show', 'index', 'create', 'order']
-  
+  resources :recipes, only: ['show', 'index', 'create', 'order', 'random']
+  get '/random', to: 'recipes#random'
 end
+
+
