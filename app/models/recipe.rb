72b03882135537c454d0b98e79080661f185db81
Emanuel Toledo <ematolr@gmail.com>
2020-06-25 12:36:47 -0500

    modified the references to create ingredients for our seeds

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
-  has_many :reviews 
-  has_many :recipe_categories
+  has_many :reviews, dependent: :destroy
+  has_many :recipe_categories, dependent: :destroy
   has_many :categories, through: :recipe_categories
   has_many_attached :images
+  has_many :portions, dependent: :destroy
 end
