618bf809be9867e24f9c8984c8a3b4130ed734ed
Bhanu Gummadi <prakash@codejek.com>
2020-06-30 15:05:33 -0700

    Javascript Changes to address Order and Cook now

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,10 +25,15 @@ require("channels")
 // External imports
 import "bootstrap";
 
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import {listenToCheckboxes} from '../plugins/ingredientSelection.js';
+
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  listenToCheckboxes();
 });
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :recipes, only: [:index, :show, :new, :create, :order] do
+  resources :recipes, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create, :index]
   end
   
   get '/random', to: 'recipes#random'
   get 'recipes/:id/instruction', to: 'recipes#instruction', as: :instruction
-
+  get 'recipes/:id/order', to: 'recipes#order', as: :order
 end
 
 
