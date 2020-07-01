8110910f5269f396c7d6f57171addf3b692f5d1d
Gonzalo Carrillo <ggcarrillo1023@gmail.com>
2020-07-01 13:44:10 -0400

    fixed review stars and some fixes with button color

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,6 +23,7 @@ require("channels")
 // ----------------------------------------------------
 
 // External imports
+import "jquery";
 import "bootstrap";
 
 
@@ -30,10 +31,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import {listenToCheckboxes} from '../plugins/ingredientSelection.js';
 
+import { initStarRating } from '../plugins/init_star_rating';
 
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   listenToCheckboxes();
+  initStarRating();
 });
--- app/javascript/plugins/ingredientSelection.js
+++ app/javascript/plugins/ingredientSelection.js
@@ -1,19 +1,19 @@
 
 function areChecked() {
-    // Get all the checkboxes in your HTML. 
+    // Get all the checkboxes in your HTML.
     const checkboxes = document.querySelectorAll('.Accept');
     // Check if all have been checked
     const checkboxarray = Array.from(checkboxes)
     const allChecked = checkboxarray.every(checkboxElement => checkboxElement.checked);
-    
+
     return allChecked;
   };
- 
+
   function handleCheckboxChange(event) {
     //Check if all have been checked and depending on the output toggle the buttons
     const orderButton = document.getElementById('order-now-button');
     const cookButton = document.getElementById('cook-now-button');
-    
+
     console.log(areChecked())
 
     if (areChecked()) {
@@ -25,19 +25,19 @@ function areChecked() {
         orderButton.style.display = 'block;'
       }
     };
-    
+
 
     function listenToCheckboxes() {
-        // Get all the checkboxes in your HTML. 
+        // Get all the checkboxes in your HTML.
         const checkboxes = document.querySelectorAll('.Accept');
         // Add event listenters to them
         checkboxes.forEach(checkboxElement => {
           checkboxElement.addEventListener('change', handleCheckboxChange);
         });
-        
-      
+
+
       };
-    
+
 
 
       export { listenToCheckboxes }
