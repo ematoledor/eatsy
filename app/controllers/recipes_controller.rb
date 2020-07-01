3d6363e4b47716075cda0d9c00d298eb8eb0b032
Emanuel Toledo <ematolr@gmail.com>
2020-07-01 16:56:12 -0500

    indented recipe_controller, modified instruction page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,61 +2,55 @@ class RecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :random ]
 
   def index
-        @recipes = Recipe.all
-    end
+  	@recipes = Recipe.all
+  end
 
   def show
-        @recipe = Recipe.find(params[:id])
+    @recipe = Recipe.find(params[:id])
 
-        @review = Review.where(recipe_id: @recipe.id)
-        @portions = Portion.where(recipe_id: @recipe.id)
+    @review = Review.where(recipe_id: @recipe.id)
+    @portions = Portion.where(recipe_id: @recipe.id)
 
-    end
+  end
 
-    def random
-        @recipe = Recipe.all.shuffle[0]
-        redirect_to recipe_path(@recipe)
-    end
+  def random
+    @recipe = Recipe.all.shuffle[0]
+  	redirect_to recipe_path(@recipe)
+  end
 
-    def new
-        @recipe = Recipe.new
-    end
+  def new
+    @recipe = Recipe.new
+  end
 
-    def create
-        @recipe = Recipe.new(recipe_params)
-        @recipe.user = current_user
-        @recipe.save
+  def create
+  	@recipe = Recipe.new(recipe_params)
+  	@recipe.user = current_user
+  	@recipe.save
+  end
 
+  def instruction
+  	@recipe = Recipe.find(params[:id])
+    @portions = Portion.where(recipe_id: @recipe.id)     
+	end
 
-    end
-
-    def instruction
-        @recipe = Recipe.find(params[:id])
-        @portions = Portion.where(recipe_id: @recipe.id)
+  def order
         
-    end
+  	@recipe = Recipe.find(params[:id])
+    @portions = Portion.where(recipe_id: @recipe.id)
+    @missing_ingredients =[]  
+  end
 
-    def order
-        
-        @recipe = Recipe.find(params[:id])
-        @portions = Portion.where(recipe_id: @recipe.id)
-        @missing_ingredients =[]
+  def thankyou
 
-        
-          
-    end
+  end
 
-    def thankyou
+  def accountsummary
 
-    end
+  end
 
-    def accountsummary
+  private
 
-    end
-
-    private
-
-    def recipe_params
-        params.require(:recipe).permit(:title, :instructions, :prep_time, :vegetarian, :vegan, :gluten_free, :dairy_free, :very_healthy, :ketogenic, :servings, :source_id, :summary, images: [])
-    end
+  def recipe_params
+    params.require(:recipe).permit(:title, :instructions, :prep_time, :vegetarian, :vegan, :gluten_free, :dairy_free, :very_healthy, :ketogenic, :servings, :source_id, :summary, images: [])
+  end
 end
